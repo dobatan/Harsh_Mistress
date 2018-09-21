57c90e35ac63c422cbda9a3e3d1233e3b7af87a3
dobatan <43465453+dobatan@users.noreply.github.com>
2018-09-21 18:18:47 +0900

    Update Struct&Define.h

--- Struct&Define.h
+++ Struct&Define.h
@@ -1,13 +1,13 @@
 #ifndef STRUCTURE_AND_DEFINE
 #define STRUCTURE_AND_DEFINE
 
-//\‘¢‘Ì‚Ì’è‹`
+//Define struct
 #include "struct.h"
 
-//’è”‚Ìİ’è
+//Setting constants
 #include "Def-Physics.h"
 
-//ƒOƒ[ƒoƒ‹•Ï”
+//Global variables
 extern int gen;
 extern int trial;
 extern int argos;
@@ -32,14 +32,14 @@ extern int nest_x;
 extern int nest_y;
 
 
-extern int step;		//ƒXƒeƒbƒv”‚Ì•\¦
+extern int step;		//Display #steps
 extern int t;
 extern int t_1;
-extern int view_z;		//‹“_‚ÌZ²•ûŒü
+extern int view_z;		//Z-axis of viewpoint
 extern int act_trace;
 extern double robot_v;
-extern double c_eva;	//ö”­ŒW”
-extern double dif;		//ŠgUŒW”
+extern double c_eva;	//Evaporation coef.
+extern double dif;		//Diffucion coef.
 extern int dA_p;
 extern int dF_p;
 extern char dir_name[1000];
@@ -48,21 +48,21 @@ extern FILE *result;
 
 
 //Log
-extern int Lay_down_a;					//Food_a‚Ì•~İ‰ñ”
-extern int Reinforce_a;					//Food_a‚Ì‹­‰»‰ñ”
-extern int Lay_down_b;					//Food_b‚Ì•~İ‰ñ”
-extern int Reinforce_b;					//Food_b‚Ì‹­‰»‰ñ”
-extern unsigned long Collision_total;	//Õ“Ë‰ñ”‚ÌƒJƒEƒ“ƒg
+extern int Lay_down_a;					//# Laying down for Food_a
+extern int Reinforce_a;					//# Reinforcing for Food_a
+extern int Lay_down_b;					//# Laying down for Food_b
+extern int Reinforce_b;					//# Reinforcing for Food_b
+extern unsigned long Collision_total;	//Counting #collision events
 
-//”z—ñiÀŒvZj
-extern Robot robot[1000];									//Robot”•ª‚Ì”z—ñ‚Ìì¬
-extern Robot_mem robot_mem[1000];							//Robot”•ª‚Ì”z—ñ‚Ìì¬
+//é…åˆ—ï¼ˆå®Ÿè¨ˆç®—ï¼‰
+extern Robot robot[1000];									//Creating array of the length = #Robots
+extern Robot_mem robot_mem[1000];							//Creating array of the length = #Robots
 extern SA sa[100];
-extern P_grid1 p_grid1[WIDTH][HEIGHT][2];					//Field‚ğƒZƒ‹‚É•ªŠ„‚·‚é
-extern P_grid2 p_grid2[WIDTH/GRID_SPAN][HEIGHT/GRID_SPAN];	//Field‚ğƒZƒ‹‚É•ªŠ„‚·‚é
-extern Food food[100];										//Food‚Ì\‘¢‘Ì
+extern P_grid1 p_grid1[WIDTH][HEIGHT][2];					//Splitting field into cells
+extern P_grid2 p_grid2[WIDTH/GRID_SPAN][HEIGHT/GRID_SPAN];	//Splitting field into cells
+extern Food food[100];										//Food Struct
 
-////”z—ñiLog•Û‘¶j
+////Array (save Log)
 //extern Robot_Log robot_log[1000][TimeLimit];
 extern Food_Log food_log[100][TimeLimit];
 extern int state_log[TimeLimit][1000];
