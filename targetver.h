dae16a0ffee53b474462b7b7ba9fe3b63ffeb995
dobatan <43465453+dobatan@users.noreply.github.com>
2018-09-21 18:44:30 +0900

    Update targetver.h

--- targetver.h
+++ targetver.h
@@ -1,8 +1,8 @@
 #pragma once
 
-// SDKDDKVer.h をインクルードすると、利用できる最も上位の Windows プラットフォームが定義されます。
+// [automatically generated note] SDKDDKVer.h
 
-// 以前の Windows プラットフォーム用にアプリケーションをビルドする場合は、WinSDKVer.h をインクルードし、
-// SDKDDKVer.h をインクルードする前に、サポート対象とするプラットフォームを示すように _WIN32_WINNT マクロを設定します。
+// [automatically generated note] WinSDKVer.h
+// [automatically generated note] SDKDDKVer.h _WIN32_WINNT
 
 #include <SDKDDKVer.h>
