2804ba2fe3caeaa2b0c77a071730c520b23369ac
RyusukeFujisawa <41564551+RyusukeFujisawa@users.noreply.github.com>
2018-09-21 16:44:04 +0900

    Update Draw-Field.c

--- Draw-Field.c
+++ Draw-Field.c
@@ -1,48 +1,44 @@
 #include "stdafx.h"
 
 
-void draw_field(void)
-{
-	//宣言
+void draw_field(void){
 	int i;
 	int x;
 	int y;
 
-
-	//設定
 	glDisable(GL_LIGHTING);
 
-	//Fieldの描画
-	glColor3d(0.0, 0.0, 0.0);//これから描画するものの色を指定
+	//Draw Field
+	glColor3d(0.0, 0.0, 0.0);
 	glBegin(GL_LINES);
 
 	for (y = 0; y < HEIGHT / 10; y++){
 		for (x = 0; x < WIDTH / 10; x++){
-			//x軸方向
-			glVertex3d(x*10,		y*10,			0);	//x軸下辺
-			glVertex3d(x*10+10,		y*10,			0);
-			glVertex3d(x*10,		y*10+10,		0);	//x軸上辺
-			glVertex3d(x*10+10,		y*10+10,		0);
-			//y軸方向			
-			glVertex3d(x*10,		y*10,			0);	//y軸左辺
-			glVertex3d(x*10,		y*10+10,		0);
-			glVertex3d(x*10+10,		y*10,			0);	//y軸右辺
-			glVertex3d(x*10+10,		y*10+10,		0);
+			//x axis
+			glVertex3d(x*10,	y*10,		0);
+			glVertex3d(x*10+10,	y*10,		0);
+			glVertex3d(x*10,	y*10+10,	0);
+			glVertex3d(x*10+10,	y*10+10,	0);
+			//y axis			
+			glVertex3d(x*10,	y*10,		0);
+			glVertex3d(x*10,	y*10+10,	0);
+			glVertex3d(x*10+10,	y*10,		0);
+			glVertex3d(x*10+10,	y*10+10,	0);
 		}
 	}
 	glEnd();
 
-	//升目の描画
-	glColor3d(0.5, 0.5, 0.5);//これから描画するものの色を指定
+	//Draw Grids
+	glColor3d(0.5, 0.5, 0.5);
 	for (y = 0; y < HEIGHT / 10; ++y){
 		for (x = 0; x < WIDTH / 10; ++x){
 			for (i = 0; i < WIDTH; ++i){
 				if (y*5+x == 2*i){
 					glBegin(GL_POLYGON);
-						glVertex3d(x*10,		y*10,			0);
-				 		glVertex3d(x*10+10,		y*10,			0);
-						glVertex3d(x*10+10,		y*10+10,		0);
-						glVertex3d(x*10,		y*10+10,		0);
+						glVertex3d(x*10,	y*10,		0);
+				 		glVertex3d(x*10+10,	y*10,		0);
+						glVertex3d(x*10+10,	y*10+10,	0);
+						glVertex3d(x*10,	y*10+10,	0);
 					glEnd();
 				}
 			}
@@ -52,22 +48,20 @@ void draw_field(void)
 }
 
 
-void draw_nest1(void)//Nest
-{
+void draw_nest1(void){
 	int i;
 
-	//描画
+	//Draw Nest
 	glPushMatrix();
-		glTranslated(nest_x, nest_y, -G1_scale + 1);	//並進
-		glRotated(0.0, 0.0, 0.0, 1.0);												//回転
-		glRotated(90.0, 1.0, 0.0, 0.0);												//Z軸を上に
+		glTranslated(nest_x, nest_y, -G1_scale + 1);
+		glRotated(0.0, 0.0, 0.0, 1.0);
+		glRotated(90.0, 1.0, 0.0, 0.0);
 		glScaled(G1_scale, G1_scale, G1_scale);
 
 		glEnable(GL_NORMALIZE);
 
 		if(opposite == 0){
-			//でかいまま
-			glColor4d(1.0, 0.0, 0.0, 0.5);//Objectの色の設定
+			glColor4d(1.0, 0.0, 0.0, 0.5);
 			glBegin(GL_POLYGON);
 			glNormal3f(0.0, 1.0, 0.0);
 			glVertex3f(0,1,0);
@@ -78,8 +72,7 @@ void draw_nest1(void)//Nest
 		}
 
 		else if(opposite == 1){
-			//左下用
-			glColor4d(1.0, 0.0, 0.0, 0.5);//Objectの色の設定
+			glColor4d(1.0, 0.0, 0.0, 0.5);
 			glBegin(GL_POLYGON);
 				glNormal3f(0.0, 1.0, 0.0);
 				glVertex3f(0,1,0);
